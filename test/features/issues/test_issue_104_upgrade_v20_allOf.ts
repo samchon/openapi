ce1c7dc172132efc14c0c03e2f706d03737673fa
Jeongho Nam <samchon.github@gmail.com>
2024-12-04 22:08:54 +0900

    Test #104 once more

--- test/features/issues/test_issue_104_upgrade_v20_allOf.ts
+++ test/features/issues/test_issue_104_upgrade_v20_allOf.ts
@@ -1,5 +1,10 @@
 import { TestValidator } from "@nestia/e2e";
-import { OpenApi, SwaggerV2 } from "@samchon/openapi";
+import {
+  HttpLlm,
+  IHttpLlmApplication,
+  OpenApi,
+  SwaggerV2,
+} from "@samchon/openapi";
 import fs from "fs";
 import typia from "typia";
 
@@ -18,4 +23,10 @@ export const test_issue_104_upgrade_v20_allOf = async (): Promise<void> => {
   TestValidator.predicate("allOf")(
     () => JSON.stringify(document).indexOf("#/definitions") === -1,
   );
+
+  const app: IHttpLlmApplication<"claude"> = HttpLlm.application({
+    model: "claude",
+    document,
+  });
+  TestValidator.equals("errors")(app.errors.length)(0);
 };
