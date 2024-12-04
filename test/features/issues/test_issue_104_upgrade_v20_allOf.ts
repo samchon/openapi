fe4f81ebcfe77d44bb36457b38dfb1a6adcc5c9d
Jeongho Nam <samchon.github@gmail.com>
2024-12-04 15:11:35 +0900

    Fix #104: conversion OpenAPI version when `allOf.length` is 1.

---
+++ test/features/issues/test_issue_104_upgrade_v20_allOf.ts
@@ -0,0 +1,21 @@
+import { TestValidator } from "@nestia/e2e";
+import { OpenApi, SwaggerV2 } from "@samchon/openapi";
+import fs from "fs";
+import typia from "typia";
+
+import { TestGlobal } from "../../TestGlobal";
+
+export const test_issue_104_upgrade_v20_allOf = async (): Promise<void> => {
+  const swagger: SwaggerV2.IDocument = typia.assert<SwaggerV2.IDocument>(
+    JSON.parse(
+      await fs.promises.readFile(
+        `${TestGlobal.ROOT}/examples/v2.0/semanticscholar.json`,
+        "utf8",
+      ),
+    ),
+  );
+  const document: OpenApi.IDocument = OpenApi.convert(swagger);
+  TestValidator.predicate("allOf")(
+    () => JSON.stringify(document).indexOf("#/definitions") === -1,
+  );
+};
